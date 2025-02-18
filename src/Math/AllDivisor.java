8128a446ed2e4f861d4d772ae037f284e28a34c3
Rahul Patel <gurjarpatel118@gmail.com>
2025-02-18 16:53:43 +0530

    Primecheck

--- src/Math/AllDivisor.java
+++ src/Math/AllDivisor.java
@@ -2,14 +2,12 @@ package Math;
 
 public class AllDivisor {
     public static void main(String[] args) {
-        int num = 12;
+        int num = 36;
         int count = 0;
         System.out.println("Factors:");
         for(int i = 1; i * i <=num; i++){
-            if(num % i == 0) count++;
-            if((num % i) == 0) count++;
+            if(num % i == 0) System.out.println(i);
+            if(i != num/i) System.out.println(num/i);
         }
-        if(count == 2) System.out.println("true");
-        else System.out.println("false");
     }
 }
