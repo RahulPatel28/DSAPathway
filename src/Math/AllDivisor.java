3f31215f9ded5ee1259f8d41f3f6c356fbe24925
Rahul Patel <gurjarpatel118@gmail.com>
2025-02-18 16:43:23 +0530

    AllDivisors

---
+++ src/Math/AllDivisor.java
@@ -0,0 +1,12 @@
+package Math;
+
+public class AllDivisor {
+    public static void main(String[] args) {
+        int num = 36;
+        System.out.println("Factors:");
+        for(int i = 1; i * i <=num; i++){
+            if(num % i == 0) System.out.println(i);
+            if(i != num/i) System.out.println(num/i);
+        }
+    }
+}
