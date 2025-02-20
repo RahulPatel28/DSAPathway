c92b286635353b845b64ac50a2ee368e7a724e80
Rahul Patel <gurjarpatel118@gmail.com>
2025-02-20 09:40:10 +0530

    CheckSortedOrNot

---
+++ src/Array/CheckSortedOrNot.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class CheckSortedOrNot {
+    public static boolean isSorted(int[] arr){
+        int count = 0;
+        int n = arr.length;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > arr[(i+1) % n] )count++;
+            if(count > 1)return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4};
+        System.out.println(isSorted(arr));
+    }
+}
