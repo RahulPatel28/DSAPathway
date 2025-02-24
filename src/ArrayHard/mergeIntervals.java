aae1c12ae946e8f32d466b6ffbfebdc16a7adf45
Rahul Patel <gurjarpatel118@gmail.com>
2025-02-24 23:30:28 +0530

    mergeIntervals

---
+++ src/ArrayHard/mergeIntervals.java
@@ -0,0 +1,19 @@
+package ArrayHard;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class mergeIntervals {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        ArrayList<int []> merge = new ArrayList<>();
+        for(int [] interval : intervals) {
+            if(merge.isEmpty() || merge.get(merge.size()-1)[1] < interval[0]) {
+                merge.add(interval);
+            }else {
+                merge.get(merge.size()-1)[1] = Math.max(merge.get(merge.size()-1)[1], interval[1]);
+            }
+        }
+        return merge.toArray(new int[merge.size()][]);
+    }
+}
