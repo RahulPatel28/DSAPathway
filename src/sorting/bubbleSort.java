beefbfc9c338b85e6b5faaff94ee47ce6e533909
Rahul Patel <gurjarpatel118@gmail.com>
2025-03-01 21:52:15 +0530

    bubbleSort

---
+++ src/sorting/bubbleSort.java
@@ -0,0 +1,16 @@
+package sorting;
+
+public class bubbleSort {
+    public static void bubbleSorting(int arr[]) {
+        int n = arr.length;
+        for(int i = 0; i < n ; i++){
+            for(int j = 0; j <n- i -1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}
