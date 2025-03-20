6b5a11a522ea53dec7fe28962c5478a7ec0c9d1f
Rahul Patel <gurjarpatel118@gmail.com>
2025-03-20 17:59:37 +0530

    detectCycleInLL

---
+++ src/LinkedList/DetectCycle.java
@@ -0,0 +1,20 @@
+package LinkedList;
+
+
+public class DetectCycle {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)return true;
+        }
+        return false;
+    }
+}
