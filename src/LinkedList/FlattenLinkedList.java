28bf74bc2400fbdd9050d45cf00f58d0b2c75bbc
Rahul Patel <gurjarpatel118@gmail.com>
2025-03-22 19:09:02 +0530

    FlattenLinkedList

---
+++ src/LinkedList/FlattenLinkedList.java
@@ -0,0 +1,42 @@
+package LinkedList;
+
+public class FlattenLinkedList {
+    class Node {
+        int data;
+        Node next;
+        Node bottom;
+
+        Node(int x) {
+            data = x;
+            next = null;
+            bottom = null;
+        }
+    }
+
+    Node flatten(Node root) {
+        if (root == null || root.next == null) return root;
+
+        root.next = flatten(root.next);
+
+        root = merge(root, root.next);
+
+        return root;
+    }
+
+
+    private Node merge(Node a, Node b) {
+        if (a == null) return b;
+        if (b == null) return a;
+
+        Node result;
+        if (a.data < b.data) {
+            result = a;
+            result.bottom = merge(a.bottom, b);
+        } else {
+            result = b;
+            result.bottom = merge(a, b.bottom);
+        }
+        result.next = null;
+        return result;
+    }
+}
