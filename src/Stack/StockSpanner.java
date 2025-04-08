31b347328f0b1111485e6e692d365005a2f514a5
Rahul Patel <gurjarpatel118@gmail.com>
2025-04-08 18:38:18 +0530

    StockSpanner

---
+++ src/Stack/StockSpanner.java
@@ -0,0 +1,27 @@
+package Stack;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+class StockSpanner {
+    Stack<Integer> stack;
+    ArrayList<Integer> list;
+    public StockSpanner() {
+        stack = new Stack<>();
+        list = new ArrayList<>();
+    }
+
+    public int next(int price) {
+        int index = list.size();
+        list.add(price);
+
+        while(!stack.isEmpty() && list.get(stack.peek()) <= price){
+            stack.pop();
+        }
+
+        int span = stack.isEmpty() ? index+1 : index-stack.peek();
+        stack.push(index);
+        return span;
+    }
+}
+
