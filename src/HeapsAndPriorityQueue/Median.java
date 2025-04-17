f3efd3605818abda789211c3e5f67eeda48858ee
Rahul Patel <gurjarpatel118@gmail.com>
2025-04-17 19:03:30 +0530

    MedianFromDataStream

---
+++ src/HeapsAndPriorityQueue/Median.java
@@ -0,0 +1,40 @@
+package HeapsAndPriorityQueue;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class MedianFromDataStream {
+
+    PriorityQueue<Integer> maxHeap;
+    PriorityQueue<Integer> minHeap;
+
+    public MedianFromDataStream() {
+        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        minHeap = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+        maxHeap.add(num);
+
+        minHeap.add(maxHeap.poll());
+
+        if (maxHeap.size() < minHeap.size()) {
+            maxHeap.add(minHeap.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (maxHeap.size() == minHeap.size()) {
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        } else {
+            return maxHeap.peek();
+        }
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
