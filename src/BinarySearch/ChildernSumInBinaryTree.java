19e9569f040372f7e0c18a6eb2b8ae1622d45d44
Rahul Patel <gurjarpatel118@gmail.com>
2025-05-03 17:52:55 +0530

    ChildernSumInBinaryTree

---
+++ src/BinarySearch/ChildernSumInBinaryTree.java
@@ -0,0 +1,30 @@
+package BinaryTrees;
+
+public class ChildernSumInBinaryTree {
+    static boolean flag = true;
+
+    public static int dfs(Node root) {
+        if (root == null) return 0;
+
+
+        if (root.left == null && root.right == null)
+            return root.data;
+
+
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+
+
+        if (root.data != left + right)
+            flag = false;
+
+
+        return root.data;
+    }
+
+    public static int isSumProperty(Node root) {
+        flag = true;
+        dfs(root);
+        return flag ? 1 : 0;
+    }
+}
