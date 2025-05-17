42bd9d882a91d640bc8b9b58d953c3e59e5f636f
Rahul Patel <gurjarpatel118@gmail.com>
2025-05-17 19:04:06 +0530

    ShortestPathInUndirectedGraph

---
+++ src/Graph/ShortestPathInUndirectedGraph.java
@@ -0,0 +1,33 @@
+package Graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ShortestPathInUndirectedGraph {
+    public int[] shortestPath(ArrayList<ArrayList<Integer>> adj, int src) {
+        // code here
+        int V = adj.size();
+        int[] dist = new int[V];
+
+        Arrays.fill(dist, -1);
+
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(src);
+        dist[src] = 0;
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+
+            for (int neighbor : adj.get(node)) {
+                if (dist[neighbor] == -1) {
+                    dist[neighbor] = dist[node] + 1;
+                    q.offer(neighbor);
+                }
+            }
+        }
+
+        return dist;
+    }
+}
