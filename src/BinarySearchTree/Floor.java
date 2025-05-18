372f7354d2a80e1ac135d36079db6d58f63c6b2e
Rahul Patel <gurjarpatel118@gmail.com>
2025-05-18 18:00:26 +0530

    FloorInBST

---
+++ src/BinarySearchTree/Floor.java
@@ -0,0 +1,21 @@
+package BinarySearchTree;
+
+public class FloorInBST {
+    public static int floor(Node root, int x) {
+        // Code here
+        int ans = -1;
+
+        while (root != null) {
+            if (root.val == x) return root.val;
+
+            if (root.val < x) {
+                ans = root.val;
+                root = root.right; // try for closer floor
+            } else {
+                root = root.left;
+            }
+        }
+
+        return ans;
+    }
+}
