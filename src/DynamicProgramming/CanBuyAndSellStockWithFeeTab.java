a57f4978808b7f8ce6877b8068b5f4148fd596ad
Rahul Patel <gurjarpatel118@gmail.com>
2025-07-13 23:21:10 +0530

    CanBuyAndSellStockWithFeeTab

---
+++ src/DynamicProgramming/CanBuyAndSellStockWithFeeTab.java
@@ -0,0 +1,27 @@
+package DynamicProgramming;
+
+public class CanBuyAndSellStockWithFeeTab {
+        public int maxProfit(int[] prices, int fee) {
+            int n = prices.length;
+            int[][] dp = new int[n + 1][2];
+
+            for (int i = n - 1; i >= 0; i--) {
+                for (int canBuy = 0; canBuy <= 1; canBuy++) {
+                    int profit = 0;
+
+                    if (canBuy == 1) {
+                        int buy = -prices[i] + dp[i + 1][0];
+                        int skip = dp[i + 1][1];
+                        profit = Math.max(buy, skip);
+                    } else {
+                        int sell = prices[i] - fee + dp[i + 1][1];
+                        int skip = dp[i + 1][0];
+                        profit = Math.max(sell, skip);
+                    }
+
+                    dp[i][canBuy] = profit;
+                }
+            }
+            return dp[0][1];
+        }
+    }
